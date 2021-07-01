497598fe601fd0b9ac1e39bb57bc6c9f7845df42
Davi Carneiro <davicarneiro@ufrj.br>
2021-07-01 20:12:40 -0300

    Modificacoes na aula de importações

--- material_do_curso/scripts/02-importacao.R
+++ material_do_curso/scripts/02-importacao.R
@@ -24,20 +24,20 @@ class(as_tibble(airquality))
 # Lendo arquivos de texto -------------------------------------------------
 
 # CSV, separado por vírgula
-imdb_csv <- read_csv(file = "dados/imdb.csv")
+imdb_csv <- read_csv(file = "material_do_curso/dados/imdb.csv")
 
 # CSV, separado por ponto-e-vírgula
-imdb_csv2 <- read_csv2(file = "dados/imdb2.csv")
+imdb_csv2 <- read_csv2(file = "material_do_curso/dados/imdb2.csv")
 
 # TXT, separado por tabulação (tecla TAB)
 imdb_txt <- read_delim(file = "dados/imdb.txt", delim = "\t")
 
 # A função read_delim funciona para qualquer tipo de separador
-imdb_delim <- read_delim("dados/imdb.csv", delim = ",")
-imdb_delim <- read_delim("dados/imdb2.csv", delim = ";")
+imdb_delim <- read_delim("material_do_curso/dados/imdb.csv", delim = ",")
+imdb_delim <- read_delim("material_do_curso/dados/imdb2.csv", delim = ";")
 
 # direto da internet
-imdb_csv <- read_csv("https://raw.githubusercontent.com/curso-r/202005-r4ds-1/master/dados/imdb.csv")
+imdb_csv_url <- read_csv("https://raw.githubusercontent.com/curso-r/202005-r4ds-1/master/dados/imdb.csv")
 
 # Interface point and click do RStudio também é útil!
 
@@ -48,21 +48,27 @@ library(readxl)
 imdb_excel <- read_excel("dados/imdb.xlsx")
 excel_sheets("dados/imdb.xlsx")
 
+?read_excel
+
+read_excel("material_do_curso/dados/imdb.xlsx", sheet = "Sheet1")
+
 # Salvando dados ----------------------------------------------------------
 
 # As funções iniciam com 'write'
 
 # CSV
-write_csv(imdb, file = "imdb.csv")
+write_csv(imdb_csv, file = "imdb.csv")
 
 # Excel
 library(writexl)
-write_xlsx(imdb, path = "imdb.xlsx")
+write_xlsx(imdb_excel, path = "imdb.xlsx")
+
+?write_xlsx
 
 # O formato rds -----------------------------------------------------------
 
 # .rds são arquivos binários do R
 # Você pode salvar qualquer objeto do R em formato .rds
 
-imdb_rds <- readr::read_rds("dados/imdb.rds")
-readr::write_rds(imdb_rds, file = "dados/imdb_rds.rds")
+imdb_rds <- readr::read_rds("material_do_curso/dados/imdb.rds") #lendo o arquivo
+readr::write_rds(imdb_rds, file = "material_do_curso/dados/imdb_rds.rds") # escrevendo/salvando 
